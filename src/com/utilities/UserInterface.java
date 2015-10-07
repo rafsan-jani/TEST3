2ae9b06a034631a249c6630126c8547d52e30375
rafsan-jani <rafsan.jani@therapservices.net>
2015-10-07 17:52:00 +0600

    third commit

--- src/com/utilities/UserInterface.java
+++ src/com/utilities/UserInterface.java
@@ -4,23 +4,23 @@ package com.utilities;
  * Created by rafsan.jani on 10/7/15.
  */
 public class UserInterface {
-//    private static UserInterface ourInstance = new UserInterface();
-//
-//    public static UserInterface getInstance() {
-//        return ourInstance;
-//    }
-//
-//    private UserInterface() {
-//    }
-//    public void showHeader(){
-//        System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
-//        System.out.println("\t\t\tFacebook Simulation\t\t\t");
-//        System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
-//    }
-//    public void showStartMenu() {
-//        System.out.println("Please select from the following options...");
-//        System.out.println("1. Login");
-//        System.out.println("2. Register");
-//        System.out.println("3. Quit");
-//    }
+    private static UserInterface ourInstance = new UserInterface();
+
+    public static UserInterface getInstance() {
+        return ourInstance;
+    }
+
+    private UserInterface() {
+    }
+    public void showHeader(){
+        System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+        System.out.println("\t\t\tFacebook Simulation\t\t\t");
+        System.out.println("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+    }
+    public void showStartMenu() {
+        System.out.println("Please select from the following options...");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Quit");
+    }
 }
